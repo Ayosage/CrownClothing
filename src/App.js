f34de9a9b064a7c1de8f8f2791400fe8922e9975
Brandon Smith <brandonsmith@Brandons-MacBook-Pro.local>
2023-01-06 10:40:51 -0500

    added cart and dropdown functions

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Home from './routes/home/home.component';
 import {Routes, Route} from 'react-router-dom'
 import Navigation from './routes/Navigation/Navigation.component';
 import Auth from './routes/auth/auth.component';
-
-
+import Shop from "./routes/shop/shop.component";
 
 
 
@@ -15,6 +14,7 @@ function App() {
   <Routes>
     <Route path="/" element={<Navigation/>}>
       <Route  index element={<Home/> }/>
+      <Route path="/shop" element={<Shop/>}/>
       <Route path="/auth" element={<Auth/>}/>
     </Route>
   
--- src/routes/Navigation/Navigation.component.jsx
+++ src/routes/Navigation/Navigation.component.jsx
@@ -4,11 +4,15 @@ import "./navigation.styles.scss"
 import { UserContext } from "../../contexts/user.context";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { SignOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 
 
 export default function Navigation(){
 
     const {currentUser} =  useContext(UserContext)
+    const {isCartOpen} = useContext(CartContext)
 
    
   
@@ -22,8 +26,9 @@ export default function Navigation(){
                 <Link className="nav-link" to="/shop">SHOP</Link>
                 <Link className="nav-link" to="/contact">CONTACT</Link>
                 {currentUser ? (<span className="nav-link" onClick={SignOutUser}> SIGN OUT</span> ):( <Link className="nav-link" to="/auth"> SIGN IN</Link>)}
-                <Link className="nav-link" to="/cart">IMG</Link>
+                <CartIcon/>
             </div>
+                {isCartOpen && <CartDropdown/>}
             </div>
            
             <Outlet/>
